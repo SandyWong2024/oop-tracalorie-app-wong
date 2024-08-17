6c1619eca4edfd98f0a36ab8f5ba55027cc2fac0
Sandy Wong <sandywongbvt2024@gmail.com>
2024-08-17 01:03:59 -0700

    first commit

---
+++ OneDrive/Desktop/OOP Classes/getters-setters-classes.js
@@ -0,0 +1,39 @@
+class Person{
+    constructor(firstName, lastName){
+        this._firstName = firstName;
+        this._lastName = lastname;
+    }
+
+    get firstName(){
+        return this.capitalizeFirst(this._firstName);
+    }
+
+    set firstName(value){
+        this._firstName = this.capitalizeFirst(value);
+    }
+
+    get lastName(){
+        return this.capitalizeFirst(this._lastName);
+    }
+
+    set lastName(value){
+        this._firstName = this.capitalizeFirst(value);
+    }
+
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    capitalizeFirst(value){
+        return value.charAt(0).toUpperCase() + value.slice(1);
+    }
+}
+
+const person1 = new Person('John', 'Doe');
+console.log(person1.firstName);
+console.log(person1.lastName)
+
+person1.firstName = 'joe';
+person1.lastName = 'smith';
+console.log(person1.fullName)
+console.log(person1);
